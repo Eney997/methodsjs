df39a6c04a85fd200bc570f9e493e115594d5222
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-11-27 13:32:33 +0400

    methodsjs

---
+++ app.js
@@ -0,0 +1,173 @@
+//1) //shekreba for of loopit
+// const numbers = [1,2,3,4,5]
+
+// let sum = 0
+// for(const gela of numbers){         
+//     sum+=gela
+// }
+ 
+// console.log(sum)
+
+
+
+
+
+//2)push methodi masivi
+// let animals = ['gela','zaza','vashli']
+
+// let newAnimals = animals.push('mze')   //daemateba elementi boloshi
+// console.log(animals)
+
+
+
+
+
+//3)pop methodi masivi
+// let animals = ['gela','zaza','vashli']
+
+// let newAnimals = animals.pop('')   //amovardeba bolo elementi
+// console.log(animals)
+
+
+
+
+
+//4)unshift methodi masivi
+// let animals = ['gela','zaza','vashli']
+
+// let newAnimals = animals.unshift('mze')  //daamatebs elements tavshi
+// console.log(animals)
+
+
+
+
+
+//5)shift methodi masivi
+// let animals = ['gela','zaza','vashli']
+
+// let newAnimals = animals.shift()   //amoagdebs elements tavidan
+// console.log(animals)
+
+
+
+
+
+//6)map methodi
+// const numbers = [1,2,3,4,5]
+
+// const newNumbers = numbers.map(numbers => numbers+1)    // yvela elements daemateba +1
+
+// console.log(newNumbers)
+
+
+
+
+
+//7)for loop
+// let xili = ['vashli','msxali','atami','banani']
+
+// for (let i=0;i<xili.length;i++){   //for lopp principi
+//     console.log(xili[i])
+// }
+
+
+
+
+
+//7)while loop
+// let xili = ['vashli','msxali','atami','banani']
+
+// let i =0
+// while (i<xili.length){             //while loop principi da sintaqsi
+//     console.log(xili[i])
+//     i++
+// }
+
+
+
+//8)forEach loop
+// let sum = 0;
+// const numbers = [1,2,3,4,5];
+// numbers.forEach(myFunction);
+
+// function myFunction(item,index,arr) {  //sheikribeba yvela
+//   arr[index]= item *10;
+// }
+// console.log(sum)
+
+
+// const numbers = [65, 44, 12, 4];
+// numbers.forEach(myFunction)
+
+// function myFunction(item, index, arr) {    // gamravleba yvela wevri 10ze
+//   arr[index] = item * 10;
+// }
+// console.log(sumnumbers)
+
+
+
+//9)for in loop
+// const car = {
+//     model:'ferari',
+//     brand:'la ferari',
+//     color:'red',
+//     price:100000,
+//     year:2012
+// }
+// for (const prop in car) {
+//     console.log(prop)       //dailogeba key wordebi
+// }
+
+// for(const prop in car){
+//     console.log(car[prop])   //dailogeba valuebi
+// }
+
+// for (const prop in (car)){
+//     console.log(`tviseba${prop};mnishvneloba${car[prop]}`) yvelaferi ertad dailogeba
+// }
+
+
+
+
+
+// 10)slice method
+// let bikes = ['kawasaki','supermoto','ninja']
+
+// let newBikes = bikes.splice (1,0,'veno') 
+// console.log(bikes)
+
+
+
+
+
+
+// 11) splice method
+// let bikes = ['kawasaki','supermoto','ninja']
+// let newBikes = bikes.slice(0,1)
+// console.log(newBikes)
+
+
+
+
+
+// 12) fillter method 
+// const numbers = [1,23,4321,3211,32,14,56743,432]
+
+// const newNumbers = numbers.filter(ricxvebi =>{
+//     return ricxvebi > 21
+// })
+// console.log(newNumbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
